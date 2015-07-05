81d28f3b805a97e2cb0646ea6cc6ba4757113b90
Mohammad Islam <reefat@gmail.com>
2015-07-04 22:02:06 -0400

    Implement tasks for serve, watch, inject and styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,157 @@
-/* jshint node: true */
+/* jshint node:true, latedef:false */
 
 'use strict';
 
-var gulp = require('gulp'),
+var path = require('path'),
+    gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    gutil = require('gulp-util'),
+    inject = require('gulp-inject'),
     wiredep = require('wiredep').stream,
-    config = require('./gulp/config.json');
+    browserSync = require('browser-sync'),
+    del = require('del');
 
-gulp.task('wiredep', function() {
-  gulp.src('./src/index.html')
-    .pipe(wiredep())
-    .pipe(gulp.dest(config.paths.tmp));
+/**
+ * Configuration
+ */
+var config = {
+  paths: {
+    src: "src",
+    tmp: ".tmp",
+    dist: "dist"
+  }
+};
+
+/**
+ * Inject @imports and compile Sass
+ */
+gulp.task('styles', function() {
+  var sassOptions = {
+    outputStyle: 'expanded'
+  };
+
+  var sassFiles = gulp.src([
+    path.join(config.paths.src, 'app/**/*.scss'),
+    path.join('!' + config.paths.src, 'app/styles/**/*.scss')
+  ], { read: false });
+
+  var injectOptions = {
+    transform: function(filePath) {
+      filePath = filePath.replace(config.paths.src + '/app/', '../');
+      return '@import "' + filePath + '";';
+    },
+    addRootSlash: false,
+    starttag: '/* inject:imports */',
+    endtag: '/* endinject */'
+  };
+
+  return gulp.src(
+    path.join(config.paths.src, 'app/styles/app.scss'))
+    .pipe(inject(sassFiles, injectOptions))
+    .pipe(sass(sassOptions)).on('error', errorHandler('Sass'))
+    .pipe(gulp.dest(path.join(config.paths.tmp, 'css')));
 });
 
-// Default task
-gulp.task('default', ['wiredep']);
+/**
+ * Inject stylesheets into `bower:css` and `inject:css`
+ * and scripts into ...
+ */
+gulp.task('inject', ['styles'], function() {
+  var cssFiles = gulp.src([
+    path.join(config.paths.tmp, 'css/**/*.css')
+  ], { read: false });
+
+  // make the path relative
+  var injectOptions = {
+    ignorePath: [config.paths.tmp],
+    addRootSlash: false
+  };
+
+  return gulp.src(path.join(config.paths.src, '*.html'))
+    .pipe(inject(cssFiles, injectOptions))  // inject:css
+    .pipe(wiredep())                        // bower:css
+    .pipe(gulp.dest(config.paths.tmp));
+
+  /* Important: While using both, `wiredep` should always run after `inject` in the pipe */
+});
+
+/**
+ * Watch for changes
+ */
+gulp.task('watch', ['inject'], function() {
+
+  // Watch for change in the root htmls (i.e. index.html) or in bower.json
+  //gulp.watch([path.join(config.paths.src, '*.html'), 'bower.json'], ['inject']);
+  gulp.watch([path.join(config.paths.src, '*.html'), 'bower.json'], ['inject', browserSync.reload]);
+
+  // Watch for change in sass
+  gulp.watch(path.join(config.paths.src, 'app/**/*.scss'), function(event) {
+    if (event.type === 'changed') {
+      gulp.start('styles', browserSync.reload);
+    } else {
+      gulp.start('inject', browserSync.reload);
+    }
+  });
+
+});
+
+/**
+ * Serve via Browsersync
+ */
+gulp.task('serve', ['clean:tmp', 'watch'], function() {
+  browserSync.init({
+    server: {
+      baseDir: './.tmp',
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+});
+
+/**
+ * Delete tmp directory
+ */
+gulp.task('clean:tmp', function() {
+  del.sync([config.paths.tmp]);
+});
+
+/**
+ * Delete dist directory
+ */
+gulp.task('clean:dist', function() {
+  del.sync([config.paths.dist]);
+});
+
+/**
+ * Delete both tmp and dist directories
+ */
+gulp.task('clean', function() {
+  del.sync([config.paths.tmp, config.paths.dist]);
+});
+
+/**
+ * Build task
+ */
+gulp.task('build', [
+  'clean:dist',
+]);
+
+/**
+ * Default task
+ */
+gulp.task('default', [
+  'build'
+]);
+
+/**
+ * Common implementation for an error handler of a gulp plugin
+ * @param  {String}   title Short description of the error
+ * @return {Function}       Callback function
+ */
+function errorHandler(title) {
+  return function(err) {
+    gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+    this.emit('end');
+  };
+}
