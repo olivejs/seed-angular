d22b9d1e8b810614221eb99c7badecb406a0629d
Mohammad Islam <reefat@gmail.com>
2015-08-03 12:11:06 -0400

    Use `ui.router` instead of `ngRoute`

--- src/app/app.js
+++ src/app/app.js
@@ -3,16 +3,18 @@
   'use strict';
 
   angular
-    .module('application', ['ngRoute'])
-    .config(function($routeProvider) {
-      $routeProvider
-        .when('/', {
+    .module('application', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+        .state('home', {
+          url: '/',
           templateUrl: 'pods/home/home.html',
           controller: 'HomeController'
-        })
-        .otherwise({
-          redirectTo: '/'
         });
+
+      // For any unmatched url, redirect to /
+      $urlRouterProvider.otherwise('/');
     });
 
 })();
